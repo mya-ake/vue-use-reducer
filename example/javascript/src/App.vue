59ea391f31ee385db11be83e949b153b46f4bceb
mya-ake <make.app.ts@gmail.com>
2019-05-02 16:56:55 +0900

    chore: Add JavaScript example

---
+++ example/javascript/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>Use Reducer Sample Counter</h1>
+
+    <div>
+      <span>{{ count }}</span>
+    </div>
+    <div>
+      <button type="button" @click="increment">+1</button>
+      <button type="button" @click="decrement">-1</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { counterState, counterDispatch } from "@/store/counter";
+
+export default {
+  name: "app",
+
+  computed: {
+    count() {
+      return counterState.count;
+    }
+  },
+
+  methods: {
+    increment() {
+      counterDispatch({ type: "increment" });
+    },
+
+    decrement() {
+      counterDispatch({ type: "decrement" });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ example/javascript/src/store/counter.js
@@ -0,0 +1,25 @@
+import { useReducer } from "vue-use-reducer";
+
+const initailState = {
+  count: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - 1
+      };
+  }
+};
+
+const [state, dispatch] = useReducer(reducer, initailState);
+
+export const counterState = state;
+export const counterDispatch = dispatch;
